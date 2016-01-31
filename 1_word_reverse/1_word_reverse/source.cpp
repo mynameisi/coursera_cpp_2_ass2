c98a0d2e642515f37e43fe41824b5940c55f048e
mynameisi <test@nowhere.com>
2016-01-31 20:54:09 +0800

    1st commit

---
+++ 1_word_reverse/1_word_reverse/source.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1_word_reverse
+//
+//  Created by wgz on 16/1/31.
+//  Copyright © 2016年 wgz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
